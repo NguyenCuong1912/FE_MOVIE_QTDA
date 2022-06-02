b22fba32b51c5293f28f1c9e84c5f7aeaaf3708f
Manh <nguyenmanh26072k1@gmail.com>
2022-06-02 23:25:53 +0700

    add redux-fix actions

--- src/redux/Actions/QuanLyRapChieuAction.js
+++ src/redux/Actions/QuanLyRapChieuAction.js
@@ -1,29 +1,82 @@
-import { baseServices } from "./baseServices";
+import { quanLyRapChieuServices } from "../../services/QuanLyRapChieuServices"
+import { RAP_CHIEU_EDIT, SET_RAP_CHIEU } from './../Types/QuanLyRapChieuType';
+import { message } from 'antd';
+import { history } from "../../App";
 
-export class QuanLyRapChieuServices extends baseServices {
-    // eslint-disable-next-line no-useless-constructor
-    constructor() {
-        super();
-    };
-    layDanhSachRap = (name) => {
-        return this.get(`/cinemas?tenRap=${name}`);
+export const layDanhSachRapChieuAction = (name = '') => {
+    return async dispatch => {
+        try {
+            const result = await quanLyRapChieuServices.layDanhSachRap(name);
+            if (result.status === 200) {
+                dispatch({
+                    type: SET_RAP_CHIEU,
+                    lstRapChieu: result.data
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
-    layRapChieuTheoMaCumRap = (id) => {
-        return this.get(`/cinemas/groupID/${id}`)
-    }
-    chiTietRap = (id) => {
-        return this.get(`/cinemas/${id}`)
-    }
-    themRap = (dataCreate) => {
-        return this.post(`/cinemas`, dataCreate)
-    }
-    capNhatRap = (id, dataEdit) => {
-        return this.put(`/cinemas/${id}`, dataEdit)
-    }
-    xoaRap = (id) => {
-        return this.delete(`/cinemas/${id}`)
-    }
-
-
 }
-export const quanLyRapChieuServices = new QuanLyRapChieuServices();
+
+export const themRapAction = (dataCreate) => {
+    return async dispatch => {
+        try {
+            const result = await quanLyRapChieuServices.themRap(dataCreate);
+            if (result.status === 201) {
+                message.success("Thêm thành công");
+                history.push(`/Admin/Cinemas`);
+            }
+        } catch (error) {
+            message.error("Thất Bại")
+            console.log(error)
+        }
+    }
+}
+export const chiTietRapAction = (id) => {
+    return async dispatch => {
+        try {
+            const result = await quanLyRapChieuServices.chiTietRap(id);
+            if (result.status === 200) {
+                dispatch({
+                    type: RAP_CHIEU_EDIT,
+                    rapChieuEdit: result.data
+                })
+
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+export const capNhatRapChieuAction = (id, dataEdit) => {
+    return async dispatch => {
+        try {
+            const result = await quanLyRapChieuServices.capNhatRap(id, dataEdit);
+            if (result.status === 200) {
+                message.success("Cập nhật thành công");
+                history.push(`/Admin/Cinemas`);
+            }
+        } catch (error) {
+            message.error("Thất Bại");
+            console.log(error)
+        }
+    }
+}
+export const xoaRapChieuAction = (id) => {
+    return async dispatch => {
+        try {
+            const result = await quanLyRapChieuServices.xoaRap(id);
+            if (result.status === 200) {
+                message.success("Xóa thành công");
+                await dispatch(layDanhSachRapChieuAction());
+                history.push(`/Admin/Cinemas`);
+            } else {
+                message.error("Thất bại");
+            }
+        } catch (error) {
+            message.error("Thất bại");
+            console.log(error)
+        }
+    }
+}
